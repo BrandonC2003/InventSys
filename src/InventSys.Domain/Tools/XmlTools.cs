5ff22b575040d693fb869c1dfec9f812946682b5
BrandonC2003 <brandoncastro200314@gmail.com>
2025-04-17 00:19:08 -0600

    Agrego clase que contiene herramientas para serializar y deserializar xml

---
+++ src/InventSys.Domain/Tools/XmlTools.cs
@@ -0,0 +1,21 @@
+﻿using System.Xml.Serialization;
+
+namespace InventSys.Domain.Tools
+{
+    public static class XmlTools
+    {
+        public static T? Deserializar<T>(string xml)
+        {
+            var serializer = new XmlSerializer(typeof(T));
+            using var stringReader = new StringReader(xml);
+            return (T?)serializer.Deserialize(stringReader);
+        }
+        public static string Serializar<T>(T objeto)
+        {
+            var serializer = new XmlSerializer(typeof(T));
+            using var stringWriter = new StringWriter();
+            serializer.Serialize(stringWriter, objeto);
+            return stringWriter.ToString();
+        }
+    }
+}
