f160f9538c12811288291b96d021d4882b54d959
BrandonC2003 <brandoncastro200314@gmail.com>
2025-04-21 22:19:06 -0600

    Agrego documentación a la interfaz IVentaService

--- src/InventSys.Domain/Interfaces/IVentaService.cs
+++ src/InventSys.Domain/Interfaces/IVentaService.cs
@@ -1,10 +1,17 @@
 ﻿using InventSys.Domain.Entities;
-
+using InventSys.Domain.Exceptions;
 namespace InventSys.Domain.Interfaces
 {
     public interface IVentaService
     {
         Task<List<Venta>> ObtenerVentaAsync(int idVenta);
+
+        /// <summary>
+        /// Registra una venta en la base de datos y actualiza el stock de los productos vendidos.
+        /// </summary>
+        /// <param name="venta"></param>
+        /// <returns></returns>
+        /// <exception cref="CustomExeption"></exception>
         Task RegistrarVentaAsync(Venta venta);
     }
 }
