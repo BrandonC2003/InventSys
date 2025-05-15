93e04bbaa7be8cdfc134d63f21d4a3fe89a48c50
BrandonC2003 <brandoncastro200314@gmail.com>
2025-05-14 21:28:54 -0600

    Inyeccion de dependencias del worker service de notificaciones

--- src/InventSysNotificationService/Program.cs
+++ src/InventSysNotificationService/Program.cs
@@ -1,7 +1,23 @@
 using InventSysNotificationService;
+using Microsoft.EntityFrameworkCore;
+using InventSys.Infrastructure.Data.EntityFramework;
+using InventSys.Domain.Interfaces;
+using InventSys.Infrastructure.Services;
+using InventSys.Application.UseCases;
 
 var builder = Host.CreateApplicationBuilder(args);
 builder.Services.AddHostedService<Worker>();
+
+builder.Services.AddDbContext<InventSysDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("InventSysDb")));
+
+builder.Services.AddTransient<IUsuarioService, UsuarioService>();
+builder.Services.AddTransient<IProductoService, ProductoService>();
+builder.Services.AddTransient<ITrazaAlertaService, TrazaAlertaService>();
+
+builder.Services.AddTransient<UsuarioUseCase>();
+builder.Services.AddTransient<ProductoUseCase>();
+builder.Services.AddTransient<TrazaAlertaUseCase>();
 
 var host = builder.Build();
 host.Run();
