964e04eaa534eb085ed995d07c330a9b0e0f5f8b
BrandonC2003 <brandoncastro200314@gmail.com>
2025-05-19 22:33:45 -0600

    Creación de pantalla de Proveedores

---
+++ src/InventSys.Application/DTOs/CrearProveedorDto.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace InventSys.Application.DTOs
+{
+    public class CrearProveedorDto
+    {
+        [Required(ErrorMessage = "Ingresa el nombre dle proveedor")]
+        public string NombreProveedor { get; set; } = null!;
+
+        [EmailAddress(ErrorMessage = "El correo ingresado no tiene un formato válido")]
+        public string? CorreoElectronico { get; set; }
+
+        public string? Telefono { get; set; }
+    }
+}
