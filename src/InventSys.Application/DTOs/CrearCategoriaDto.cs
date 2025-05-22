4e7b11a3f59006cef74cd1100bedd962fd1dfede
BrandonC2003 <brandoncastro200314@gmail.com>
2025-05-21 21:17:01 -0600

    Finalización de la pantalla de categorías

---
+++ src/InventSys.Application/DTOs/CrearCategoriaDto.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace InventSys.Application.DTOs
+{
+    public class CrearCategoriaDto
+    {
+        [Required(ErrorMessage = "Ingresa el nombre de la categoría.")]
+        public string NombreCategoria { get; set; }
+        public string? Descripcion { get; set; }
+    }
+}
